034e4b40532f6ea7f0bc728b6ba933caa204d935
aaltoan <antti.aalto@hiq.fi>
2014-04-26 14:30:39 +0800

    project init

---
+++ desktop/src/hk/ust/flappyheart/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package hk.ust.flappyheart.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import hk.ust.flappyheart.FlappyHeart;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyHeart(), config);
+	}
+}
