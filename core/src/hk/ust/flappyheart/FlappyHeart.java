154682c503f4aa5a92e1d5caa66d42741b0016e6
aaltoan <antti.aalto@hiq.fi>
2014-04-26 23:05:24 +0800

    heart beat background service

--- core/src/hk/ust/flappyheart/FlappyHeart.java
+++ core/src/hk/ust/flappyheart/FlappyHeart.java
@@ -1,5 +1,7 @@
 package hk.ust.flappyheart;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import hk.ust.helper.AssetLoader;
 import hk.ust.screen.SplashScreen;
 
@@ -8,18 +10,27 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyHeart extends Game {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create() {
-		AssetLoader.load();
-		setScreen(new SplashScreen(this));
-	}
+    SpriteBatch batch;
+    Texture img;
+    private AtomicInteger bpm;
+    
+    public FlappyHeart(AtomicInteger bpm) {
+        this.bpm = bpm;
+    }
+    
+    public int getBpm() {
+        return bpm.get();
+    }
 
-	@Override
-	public void dispose() {
-		super.dispose();
-		AssetLoader.dispose();
-	}
+    @Override
+    public void create() {
+        AssetLoader.load();
+        setScreen(new SplashScreen(this));
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        AssetLoader.dispose();
+    }
 }
