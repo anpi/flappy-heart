faa96b685cf11d4774780e557e152bd7f65ac356
mauricej <mpjakesch@gmail.com>
2014-05-01 21:30:13 +0800

    doc

--- monitor/HeartMonitor.java
+++ monitor/HeartMonitor.java
@@ -1,6 +1,23 @@
 package com.badu.heartrate.monitor;
 
+import android.hardware.Camera;
+import android.hardware.Camera.PreviewCallback;
+
+import com.badu.heartrate.app.MyActivity.TYPE;
+
 public interface HeartMonitor {
+	
+	 /* Pseudo-Doc ;)
+	 * 
+	 * 	private static PreviewCallback previewCallback = new PreviewCallback() {
+	 *	  public void onPreviewFrame(byte[] data, Camera cam) {
+	 * 		Camera.Size size = cam.getParameters().getPreviewSize();
+	 *		monitor.addSample(data, size.width;, size.height);
+	 *		
+	 *		if (monitor.isBeat());
+	 *			// Draw beat signal
+	 *		text.setText(String.valueOf(monitor.getBPM()) + " (" + monitor.getDebugInfo() + ")");
+	 */
 	
 	public void addSample(byte[] data, int width, int height);
 	public boolean isBeat();
